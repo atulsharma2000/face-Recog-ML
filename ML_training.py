4e061b5eaddd38908a8489acec03f7ceca105f5f
Atul Sharma <41663027+atulsharma2000@users.noreply.github.com>
2020-05-20 20:26:02 +0530

    Create ML_training.py

---
+++ ML_training.py
@@ -0,0 +1,93 @@
+from keras.applications import VGG16
+img_rows = 224
+img_cols = 224 
+# Re-loads the VGG16 model without the top or FC layers
+model = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+for layer in model.layers:
+    layer.trainable = False
+for (i,layer) in enumerate(model.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+def addTopModel(bottom_model):
+    """creates the top or head of the model that will be 
+    placed ontop of the bottom layers"""
+    top_model = bottom_model.output
+    top_model = Flatten(name = "flatten")(top_model)
+    top_model = Dense(128, activation = "relu")(top_model)
+    top_model = Dense(2, activation = "softmax")(top_model)
+    return top_model
+    from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.models import Model
+
+modelnew = Model(inputs=model.input, outputs=addTopModel(model))
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_data_dir = 'dataset/train/'
+validation_data_dir = 'dataset/validation/'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_batchsize = 16
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+                   
+checkpoint = ModelCheckpoint("FaceRecognition.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+# Note we use a very small learning rate 
+modelnew.compile(loss = 'binary_crossentropy',
+              optimizer = Adam(),
+              metrics = ['accuracy'])
+
+nb_train_samples = 520
+nb_validation_samples = 80
+epochs = 3
+batch_size = 16
+
+history = modelnew.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+modelnew.save("FaceRecognition.h5")
