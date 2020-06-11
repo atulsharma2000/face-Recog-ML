ea9a5e66b2d203a1fabf797b831d11747943895a
Atul Sharma <41663027+atulsharma2000@users.noreply.github.com>
2020-06-11 13:16:12 +0530

    Update and rename ML_dataset.py to dataset.py

--- dataset.py
+++ dataset.py
@@ -1,19 +1,14 @@
 import cv2
 import numpy as np
-
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def face_extractor(img):
-    
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-    
     if faces is ():
         return None
-
     for (x,y,w,h) in faces:
         cropped_face = img[y:y+h, x:x+w]
-
     return cropped_face
 
 cap = cv2.VideoCapture(0)
